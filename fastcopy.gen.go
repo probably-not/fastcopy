f905db499c051d12c41df9f50b2967a5493f73e3
Coby Benveniste <coby.b@arpeely.com>
2021-08-23 19:57:01 +0300

    Generate

---
+++ fastcopy.gen.go
@@ -0,0 +1,165 @@
+/*
+* CODE GENERATED AUTOMATICALLY WITH github.com/probably-not/fastcopy/cmd/gen.go
+*/
+
+package fastcopy
+
+func copybyte1(dst, src []byte) {
+	*(*[1]byte)(dst) = *(*[1]byte)(src)
+}
+
+func copybyte2(dst, src []byte) {
+	*(*[2]byte)(dst) = *(*[2]byte)(src)
+}
+
+func copybyte3(dst, src []byte) {
+	*(*[3]byte)(dst) = *(*[3]byte)(src)
+}
+
+func copybyte4(dst, src []byte) {
+	*(*[4]byte)(dst) = *(*[4]byte)(src)
+}
+
+func copybyte5(dst, src []byte) {
+	*(*[5]byte)(dst) = *(*[5]byte)(src)
+}
+
+func copybyte6(dst, src []byte) {
+	*(*[6]byte)(dst) = *(*[6]byte)(src)
+}
+
+func copybyte7(dst, src []byte) {
+	*(*[7]byte)(dst) = *(*[7]byte)(src)
+}
+
+func copybyte8(dst, src []byte) {
+	*(*[8]byte)(dst) = *(*[8]byte)(src)
+}
+
+func copybyte9(dst, src []byte) {
+	*(*[9]byte)(dst) = *(*[9]byte)(src)
+}
+
+func copybyte10(dst, src []byte) {
+	*(*[10]byte)(dst) = *(*[10]byte)(src)
+}
+
+func copystring1(dst, src []string) {
+	*(*[1]string)(dst) = *(*[1]string)(src)
+}
+
+func copystring2(dst, src []string) {
+	*(*[2]string)(dst) = *(*[2]string)(src)
+}
+
+func copystring3(dst, src []string) {
+	*(*[3]string)(dst) = *(*[3]string)(src)
+}
+
+func copystring4(dst, src []string) {
+	*(*[4]string)(dst) = *(*[4]string)(src)
+}
+
+func copystring5(dst, src []string) {
+	*(*[5]string)(dst) = *(*[5]string)(src)
+}
+
+func copystring6(dst, src []string) {
+	*(*[6]string)(dst) = *(*[6]string)(src)
+}
+
+func copystring7(dst, src []string) {
+	*(*[7]string)(dst) = *(*[7]string)(src)
+}
+
+func copystring8(dst, src []string) {
+	*(*[8]string)(dst) = *(*[8]string)(src)
+}
+
+func copystring9(dst, src []string) {
+	*(*[9]string)(dst) = *(*[9]string)(src)
+}
+
+func copystring10(dst, src []string) {
+	*(*[10]string)(dst) = *(*[10]string)(src)
+}
+
+func copyint1(dst, src []int) {
+	*(*[1]int)(dst) = *(*[1]int)(src)
+}
+
+func copyint2(dst, src []int) {
+	*(*[2]int)(dst) = *(*[2]int)(src)
+}
+
+func copyint3(dst, src []int) {
+	*(*[3]int)(dst) = *(*[3]int)(src)
+}
+
+func copyint4(dst, src []int) {
+	*(*[4]int)(dst) = *(*[4]int)(src)
+}
+
+func copyint5(dst, src []int) {
+	*(*[5]int)(dst) = *(*[5]int)(src)
+}
+
+func copyint6(dst, src []int) {
+	*(*[6]int)(dst) = *(*[6]int)(src)
+}
+
+func copyint7(dst, src []int) {
+	*(*[7]int)(dst) = *(*[7]int)(src)
+}
+
+func copyint8(dst, src []int) {
+	*(*[8]int)(dst) = *(*[8]int)(src)
+}
+
+func copyint9(dst, src []int) {
+	*(*[9]int)(dst) = *(*[9]int)(src)
+}
+
+func copyint10(dst, src []int) {
+	*(*[10]int)(dst) = *(*[10]int)(src)
+}
+
+func copyfloat641(dst, src []float64) {
+	*(*[1]float64)(dst) = *(*[1]float64)(src)
+}
+
+func copyfloat642(dst, src []float64) {
+	*(*[2]float64)(dst) = *(*[2]float64)(src)
+}
+
+func copyfloat643(dst, src []float64) {
+	*(*[3]float64)(dst) = *(*[3]float64)(src)
+}
+
+func copyfloat644(dst, src []float64) {
+	*(*[4]float64)(dst) = *(*[4]float64)(src)
+}
+
+func copyfloat645(dst, src []float64) {
+	*(*[5]float64)(dst) = *(*[5]float64)(src)
+}
+
+func copyfloat646(dst, src []float64) {
+	*(*[6]float64)(dst) = *(*[6]float64)(src)
+}
+
+func copyfloat647(dst, src []float64) {
+	*(*[7]float64)(dst) = *(*[7]float64)(src)
+}
+
+func copyfloat648(dst, src []float64) {
+	*(*[8]float64)(dst) = *(*[8]float64)(src)
+}
+
+func copyfloat649(dst, src []float64) {
+	*(*[9]float64)(dst) = *(*[9]float64)(src)
+}
+
+func copyfloat6410(dst, src []float64) {
+	*(*[10]float64)(dst) = *(*[10]float64)(src)
+}
